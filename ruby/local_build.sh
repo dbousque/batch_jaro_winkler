c1dafdebd2a3e88fe7402aa802b304af7c0fe53d
dbousque <bousquet.dominik@gmail.com>
2023-12-05 16:18:25 +0100

    chore(version): bump version

--- ruby/local_build.sh
+++ ruby/local_build.sh
@@ -1,1 +1,1 @@
-rm -Rf ext/batch_jaro_winkler/ext && cp -R ../lib ext/batch_jaro_winkler/ext && gem build batch_jaro_winkler.gemspec && gem install batch_jaro_winkler-0.1.2.gem
+rm -Rf ext/batch_jaro_winkler/ext && cp -R ../lib ext/batch_jaro_winkler/ext && gem build batch_jaro_winkler.gemspec && gem install batch_jaro_winkler-0.1.3.gem
--- ruby/publish.sh
+++ ruby/publish.sh
@@ -1,1 +1,1 @@
-./local_build.sh && gem push batch_jaro_winkler-0.1.2.gem
+./local_build.sh && gem push batch_jaro_winkler-0.1.3.gem
